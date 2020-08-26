c74775e7b72f45319a1cc15da1a7a537cbb31bf4
Apul <apuljain5@gmail.com>
2020-08-26 21:20:57 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,69 @@
+var ul = document.getElementById('list')
+var li;
+
+
+var addButton = document.getElementById('add')
+addButton.addEventListener('click', addItem);
+
+var removeButton = document.getElementById('remove')
+removeButton.addEventListener('click', removeItem);
+
+var clearButton = document.getElementById('clear')
+clearButton.addEventListener('click', clearItem);
+
+function addItem() {
+    var input = document.getElementById('input');
+    var item = input.value;
+    ul = document.getElementById('list');
+    var textnode = document.createTextNode(item)
+    console.log(item)
+    if (item === '') {
+        return false;
+
+    } else {
+        // create li
+        li = document.createElement('li')
+            //create checkbox
+        var checkbox = document.createElement('input')
+        checkbox.type = "checkbox";
+        checkbox.setAttribute('id', 'check')
+
+        // create label
+        var label = document.createElement('label');
+        label.setAttribute('for', 'item') //optional
+
+        // add these elements on web page
+
+        ul.appendChild(label)
+        li.appendChild(checkbox)
+        label.appendChild(textnode)
+        li.appendChild(label)
+        ul.insertBefore(li, ul.childNodes[0])
+
+        setTimeout(() => {
+            li.className = 'visual'
+        }, 100)
+
+        input.value = ''
+    }
+}
+
+
+function removeItem() {
+    li = ul.children
+    for (let index = 0; index < li.length; index++) {
+        while (li[index] && li[index].children[0].checked) {
+            ul.removeChild(li[index])
+        }
+
+    }
+}
+
+function clearItem() {
+    li = ul.children
+    for (let index = 0; index < li.length; index++) {
+        while (li[index] && (!(li[index].children[0].checked) || li[index].children[0].checked)) {
+            ul.removeChild(li[index])
+        }
+    }
+}
